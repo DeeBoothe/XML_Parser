7651fcf4c890f5b7ee0d28a5b778d59f5328becf
Dee Boothe <deidre.boothe@outlook.com>
2024-12-15 19:44:29 -0800

    catalog.xml added, new included header file

--- xml_parser.cpp
+++ xml_parser.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <libxml>
 
 using namespace std;
 
-int main({
+
+int main(){ 
+
+    ptree pt;
+
+    read_xml("", pt);
+
+    BOOST_FOREACH(ptree::value_tyoe & child, pt.get_child(""))
+
+ 
     
+
+      
     return 0;
-})
+}
